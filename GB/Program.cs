0086ca966021fbcd36b8b8ea744455b5a73c2083
LidiiaMosina <downj@inbox.ru>
2020-12-11 18:35:22 +0300

    Добавьте файлы проекта.

---
+++ GB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+            DateTime now = DateTime.Now;
+            string date = now.GetDateTimeFormats('d')[0];
+            Console.WriteLine($"Hello, {name}, today is {date}!");
+            Console.ReadLine();
+        }
+    }
+
+}
