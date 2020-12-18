6f23c558a33b31cd29efb00b69571acf172176e8
LidiiaMosina <downj@inbox.ru>
2020-12-18 18:35:09 +0300

    Lesson 2-2

--- GB/Program.cs
+++ GB/Program.cs
@@ -10,11 +10,50 @@ namespace GB
     {
         static void Main(string[] args)
         {
-            Console.Write("What's your name? ");
-            var name = Console.ReadLine();
-            DateTime now = DateTime.Now;
-            string date = now.GetDateTimeFormats('d')[0];
-            Console.WriteLine($"Hello, {name}, today is {date}!");
+            Console.Write("Введите номер месяца");
+            int numbMonth = Convert.ToInt32(Console.ReadLine());
+            string monthTitle = "";
+
+            switch (numbMonth)
+            {
+                case 1:
+                    monthTitle = "Январь";
+                    break;
+                case 2:
+                    monthTitle = "Февраль";
+                    break;
+                case 3:
+                    monthTitle = "Март";
+                    break;
+                case 4:
+                    monthTitle = "Апрель";
+                    break;
+                case 5:
+                    monthTitle = "Май";
+                    break;
+                case 6:
+                    monthTitle = "Июнь";
+                    break;
+                case 7:
+                    monthTitle = "Июль";
+                    break;
+                case 8:
+                    monthTitle = "Август";
+                    break;
+                case 9:
+                    monthTitle = "Сентябрь";
+                    break;
+                case 10:
+                    monthTitle = "Октябрь";
+                    break;
+                case 11:
+                    monthTitle = "Ноябрь";
+                    break;
+                case 12:
+                    monthTitle = "Декабрь";
+                    break;
+            }
+            Console.WriteLine($"Это {monthTitle}");
             Console.ReadLine();
         }
     }
